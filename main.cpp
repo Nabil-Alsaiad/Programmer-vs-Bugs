33ead4c60142e28e3b793edd5f54e33ae955c395
Nabil Alsaiad <nabloo2002@gmail.com>
2023-01-18 11:53:45 +0800

    Starter kit addition
    Added the starter kit provided by the doctor and made some initial changes

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: T14L
+// Names: Nabil | Hilmi | Asem
+// IDs: 1221301330 | 1221301686 | 1211306893
+// Emails: 1221301330@student.mmu.edu.my | 1221301686@student.mmu.edu.my | 1211306893@student.mmu.edu.my
+// Phones: +60 11-1174 1088 | +60 17-295 2865 | +60 00-0000 0000
+// *********************************************************
+
+#include "pf/helper.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string names = "[Nabil Alsaiad, Hilmi Zaki, Asem Maroof]";
+
+int main()
+{
+    cout << "Assignment (Part 1) by " + names << endl;
+    cout << "Let's Get Started!" << endl;
+    pf::Pause();
+
+    return 1;
+}
